14892533f9a4d15ca5f8769eefad5920bb327c6a
Noah Zajicek <nzajicek@WVWAUTHORITY.COM>
2023-09-11 14:48:32 -0400

    Modify bot/main.go file. Import tools for API

--- bot/bot.go
+++ bot/bot.go
@@ -1,12 +1,27 @@
 package bot
 
 import (
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
 	"github.com/zajicekn/dailyQuote/quote"
 )
 
-func Bot() {
-	// This variable will hold the Quote and Author
-	// from the API
+func Bot() string {
+	// This variable will hold the Quote and
+	// Author from the quote package
 	quoteMessage := quote.Quote()
 
+	// Load the .env variables
+	err := godotenv.Load(".env")
+	if err != nil {
+		cwd, _ := os.Getwd()
+		log.Println("Current working directory:", cwd)
+		log.Fatal("Error in loading .env file: ", err)
+	}
+
+	// Soon to implement API token
+
+	return quoteMessage
 }
--- main/main.go
+++ main/main.go
@@ -1,5 +1,12 @@
 package main
 
-func main() {
+import (
+	"fmt"
 
+	"github.com/zajicekn/dailyQuote/bot"
+)
+
+func main() {
+	resp := bot.Bot()
+	fmt.Println(resp)
 }
